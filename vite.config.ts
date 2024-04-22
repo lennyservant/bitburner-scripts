9eef8965b565f2986dc2b34b2d5363cf93ab9330
lennyservant <dydaniel169@gmail.com>
2024-04-22 10:53:58 +0800

    allow css to as .bcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,45 @@
 /* eslint-env node */
 import { defineConfig } from "viteburner";
 import { resolve } from "path";
+import type { Plugin } from "vite";
+
+function transformBCss() {
+	const moduleCSSFilter = /\.module\.bcss$/;
+	const cssFilter = /\.bcss$/;
+	return {
+		name: "lenny-bcss-transform",
+		transform(css, id, options) {
+			switch (true) {
+				case cssFilter.test(id): // TODO: figure out how to get typedefs from css files out
+					return {
+						code: `export default \`${css}\``,
+						map: null,
+					};
+			}
+		},
+	} satisfies Plugin;
+}
 
 export default defineConfig({
 	resolve: {
-		alias: {
-			"@": resolve(__dirname, "src"),
-			"/src": resolve(__dirname, "src"),
-			react: resolve(__dirname, "src", "vendored", "react.js"),
-			"react-dom": resolve(__dirname, "src", "vendored", "react-dom.js"),
-		},
+		// alias: {
+		// 	"@": resolve(__dirname, "src"),
+		// 	"/src": resolve(__dirname, "src"),
+		// 	react: resolve(__dirname, "src", "vendored", "react.js"),
+		// 	"react-dom": resolve(__dirname, "src", "vendored", "react-dom.js"),
+		// },
+		alias: [
+			{ find: "@", replacement: resolve(__dirname, "src") },
+			{ find: "/src", replacement: resolve(__dirname, "src") },
+			{
+				find: "react",
+				replacement: resolve(__dirname, "src", "vendored", "react.js"),
+			},
+			{
+				find: "react-dom",
+				replacement: resolve(__dirname, "src", "vendored", "react-dom.js"),
+			},
+		],
 	},
 	build: {
 		outDir: "dist",
@@ -26,12 +56,19 @@ export default defineConfig({
 					filename: file.replace(/[jt]sx?$/, "js").replace(/^src/, ""),
 				}),
 			},
+			{
+				pattern: "src/**/*.bcss",
+				transform: true, // css
+				location: (file) => ({
+					filename: file.replace(/bcss?$/, "js").replace(/^src/, ""),
+				}),
+			},
 			{ pattern: "src/**/*.{script,txt}" },
 		],
 		sourcemap: "inline",
 	},
 
-	plugins: [],
+	plugins: [transformBCss()],
 
 	esbuild: {
 		jsx: `transform`,
